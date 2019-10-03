53bb6e5ee6ebd9711207b2cc3dd3f4b6b71c4d96
inder5522 <56110745+inder5522@users.noreply.github.com>
2019-10-03 14:13:23 +0530

    Update basicfunctions.cpp
    Added some more functions like arc, ellipse, rectangle, etc

--- basicfunctions.cpp
+++ basicfunctions.cpp
@@ -9,6 +9,9 @@ int main()
     outtextxy(200,200,"We Are Venom");
     line(300,300,600,600);
     circle(400,400,6);
+    rectangle(300,300,400,400);
+    arc(100, 100, 0, 135, 50);
+    ellipse(100, 100, 0, 360, 50, 25);
     delay(5000);
     closegraph();
     return 0;
