fbbed2eb27dbbc121005ee9b798a7b30def27f0d
ANURAG KUMAR <48417630+anuragk539@users.noreply.github.com>
2019-10-05 21:56:37 +0530

    Update lineusingdda.cpp
    Added comments removed unnecessary header files and functions

--- lineusingdda.cpp
+++ lineusingdda.cpp
@@ -1,5 +1,4 @@
 #include<iostream>
-#include<cstdlib>
 #include<graphics.h>
 #include<cmath>
 using namespace std;
@@ -8,23 +7,26 @@ void DDA(int X0,int Y0, int X1, int Y1)
 {
 	int dx = X1-X0; 
     	int dy = Y1-Y0; 
+	//To count no of steps.
 	int steps = abs(dx) > abs(dy) ? abs(dx) : abs(dy);
 	float Xinc = dx / (float) steps; 
     	float Yinc = dy / (float) steps; 
 	float X = X0; 
     	float Y = Y0; 
+	//Loop to display all the points between the end points.
     	for (int i = 0; i <= steps; i++) 
     	{ 
         	putpixel (X,Y,100);  
         	X += Xinc; 
         	Y += Yinc;           
         	delay(5);
-                //system("clear");
+              
 }
 } 
 int main()
 {
 int X0,Y0,X1,Y1;
+//Input the co ordinates from the user.
 cout<<"Enter the four coordinates: "<<endl;
 cin>>X0>>Y0>>X1>>Y1;
 int gd = DETECT, gm;
