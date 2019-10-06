9d7dccd50be2925dc07dea78a82cbef4034415dd
Jaspreet Singh <44704071+singh-jp-7@users.noreply.github.com>
2019-10-06 12:43:11 +0530

    Create Textformatting.cpp
    This program prints the text in various fonts using the for loop. However this text formatting is not available in SDL so this program works good in Turbo C++.

---
+++ Textformatting.cpp
@@ -0,0 +1,27 @@
+#include <graphics.h>
+#include<iostream.h>
+void main() 
+{
+	
+	int gd=DETECT,gm;
+
+	
+	initgraph(&gd,&gm,NULL);
+
+	int x = 75, y = 75, f = 0;
+
+	
+	for (f = 0; f <= 5; f++)
+	{
+		setlinestyle(f, 0, 1);//Sets the text style
+		outtextxy(x, y, "Long way Down....");//Loop to print the text in different fonts.
+		y = y + 20;
+	}
+
+	delay(10000);
+  getch();
+	closegraph();
+  
+  }
+  
+
