305ddd6e59a69bbd38ef33bc8d994e103d8b7b52
inder5522 <56110745+inder5522@users.noreply.github.com>
2019-10-21 23:15:12 +0530

    Update pixel.cpp

--- pixel.cpp
+++ pixel.cpp
@@ -3,9 +3,13 @@
 using namespace std;
 int main()
 {
-    int gd = DETECT, gm;
+    int gd = DETECT, gm , i=0;
     initgraph(&gd,&gm,NULL);
-    putpixel(100,100,100);
+    for(int i=0;i<=30;i=i+5)
+    {
+    putpixel(100,100+(i*3),500+(i*5));
+    }
+    
     delay(5000);
     closegraph();
     return 0;
